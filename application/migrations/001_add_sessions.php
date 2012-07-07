d1ce840662d6148a0362340f6b71c18945919a2a
Anthony VEREZ <netantho@gmail.com>
2012-07-07 20:06:05 +0200

    Migration: add_sessions

---
+++ application/migrations/001_add_sessions.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$this->load->dbforge();
+
+class Migration_Add_sessions extends CI_Migration {
+
+	public function up()
+	{
+		$this->db->query("
+			CREATE TABLE IF NOT EXISTS  `ci_sessions` (
+				session_id varchar(40) DEFAULT '0' NOT NULL,
+				ip_address varchar(45) DEFAULT '0' NOT NULL,
+				user_agent varchar(120) NOT NULL,
+				last_activity int(10) unsigned DEFAULT 0 NOT NULL,
+				user_data text NOT NULL,
+				PRIMARY KEY (session_id),
+				KEY `last_activity_idx` (`last_activity`)
+			);
+		");
+	}
+
+	public function down()
+	{
+		$this->dbforge->drop_table('ci_sessions');
+	}
+}
