635d96f7c6adb978b5a05345a4387fe3457ffaae
Anthony VEREZ <netantho@gmail.com>
2012-07-21 23:59:19 +0200

    Création du contrôleur All_test pour lancer tous les tests

---
+++ application/controllers/tests/all_test.php
@@ -0,0 +1,27 @@
+<?php defined("BASEPATH") or exit("No direct script access allowed");
+(defined("ENVIRONMENT") and (ENVIRONMENT == 'testing' or ENVIRONMENT == 'development'))
+or exit("No tests running in production environment");
+
+/**
+* @author Anthony VEREZ (netantho@minet.net)
+*         président de MiNET 2012-2013
+* @see http://www.anthony-verez.fr
+* @since 07/2012
+*/
+
+class All_test extends CI_Controller {
+	public function index()
+	{
+		$tests = array(
+			'adherent_model_test.php' => 'Adherent_model_test'
+		);
+
+		foreach ($tests as $file => $model)
+		{
+			require_once($file);
+
+			$objet = eval('return new Adherent_model_test();');
+			$objet->index();
+		}
+	}
+}
