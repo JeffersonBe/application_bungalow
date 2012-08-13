a888c762757607537e36ef26fc4a3641135a0939
jefferson <bonnaire.jefferson@gmail.com>
2012-08-13 17:22:26 +0200

    Ajout des images, création du design des bungalows

--- bungalow/javascripts/app.js
+++ bungalow/javascripts/app.js
@@ -1,8 +1,4 @@
 jQuery(document).ready(function ($) {
-
-        $('#slide1').parallax("center", 0, 0.1, true);
-        $('#slide2').parallax("center", 900, 0.1, true);
-        $('#slide3').parallax("center", 2900, 0.1, true);
         
   /* Use this js doc for all application specific JS */
 
